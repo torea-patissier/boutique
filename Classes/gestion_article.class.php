2de3e370623f3b65851bffad6125a59efc92ac7f
nuno.barbosa <nuno.barbosa@laplateforme.io>
2021-03-03 19:53:06 +0100

    Ajout article "terminé"

--- Classes/gestion_article.class.php
+++ Classes/gestion_article.class.php
@@ -3,10 +3,69 @@ require_once('../Classes/bdd.class.php');
 
 class GestionProduit extends bdd{
 
-    public function ajoutImgArticle()
+    function selectCategory()
     {
-        $con = $this->connectDb();
-        $req = $con->prepare("INSERT INTO image_produit (nom, taille, type, bin, id_produit) values(?, ?, ?, ?, 0)");
-        $req->execute(array($_FILES["img"]["name"], $_FILES["img"]["size"], $_FILES["img"]["type"], file_get_contents($_FILES["img"]["tmp_name"])));
+        $con = $this->connectDb(); // Connexion Db 
+        $stmt = $con->prepare("SELECT * FROM categories");// Requete
+        $stmt->execute();//J'éxécute la requete
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);//Result devient un tableau des valeurs obtenues
+
+        foreach($result as $resultat){
+            $categorie = $resultat["nom"];
+            $idCategorie = $resultat["id"];
+
+            echo "<option value='$idCategorie'>$categorie</option>";
+        
+        }
     }
+
+    function selectSCategory()
+    {
+        $con = $this->connectDb(); // Connexion Db 
+        $stmt = $con->prepare("SELECT * FROM sous_categories");// Requete
+        $stmt->execute();//J'éxécute la requete
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);//Result devient un tableau des valeurs obtenues
+
+        foreach($result as $resultat){
+            $categorie = $resultat["nom"];
+            $idCategorie = $resultat["id"];
+
+            echo "<option value='$idCategorie'>$categorie</option>";
+        
+        }
+    }
+
+    public function ajoutProduitBdd()
+    {
+        $nomProduit = htmlspecialchars($_POST['nom_produit']);
+        $prixProduit = htmlspecialchars($_POST['prix_produit']);
+        $descriptionProduit = htmlspecialchars($_POST['description_produit']);
+        $idCategorie = htmlspecialchars($_POST['Categorie']);
+        $idSCategorie = htmlspecialchars($_POST['SCategorie']);
+        $stockProduit = htmlspecialchars($_POST['stock_produit']);
+        $fileName = $_FILES["img"]["name"];
+        $fileSize = $_FILES["img"]["size"];
+        $fileType = $_FILES["img"]["type"];
+        $fileTmpName = file_get_contents($_FILES["img"]["tmp_name"]);
+        $con = $this->connectDb();
+        $req = $con->prepare("INSERT INTO produits(nom, prix, description, id_categorie, id_sous_categorie, stock, nom_image_produit, taille_image_produit, type_image_produit, bin_image_produit) values (:nom, :prix, :description, :id_categorie, :id_sous_categorie, :stock, :fileName, :fileSize, :fileType, :fileTmpName)");
+        $req->bindValue("nom", $nomProduit, PDO::PARAM_STR);
+        $req->bindValue("prix", $prixProduit, PDO::PARAM_STR);
+        $req->bindValue("description", $descriptionProduit, PDO::PARAM_STR);
+        $req->bindValue("id_categorie", $idCategorie, PDO::PARAM_INT);
+        $req->bindValue("id_sous_categorie", $idSCategorie, PDO::PARAM_INT);
+        $req->bindValue("stock", $stockProduit, PDO::PARAM_INT);
+        $req->bindValue("fileName", $fileName, PDO::PARAM_STR);
+        $req->bindValue("fileSize", $fileSize, PDO::PARAM_INT);
+        $req->bindValue("fileType", $fileType, PDO::PARAM_STR);
+        $req->bindValue("fileTmpName", $fileTmpName, PDO::PARAM_LOB);
+
+        $req->execute();
+
+
+        // , 
+        // , ?, ?, ?, ?
+        // 
+    }
+
 }
--- Gestion_article/gestion_article.php
+++ Gestion_article/gestion_article.php
@@ -14,21 +14,31 @@ $pageGestion = new GestionProduit;
 </head>
 <body>
     <main>
-        <form name="addProduit" action="gestion_article.php" method="POST">
-            <input type="text" name="nom_produit" placeholder="Nom du Produit">
-            <input type="text" name="prix_produit" placeholder="Prix du Produit">
-            <input type="text" name="nom_produit" placeholder="Nom du Produit">
-            <input type="textarea" name="description_produit" placeholder="Décrivez le Produit">
+        <form name="addProduit" action="gestion_article.php" method="POST" enctype="multipart/form-data">
+        <p>Nom du Produit :</p>
+            <input type="text" name="nom_produit" placeholder="Nom du Produit"><br />
+        <p>Prix de vente :</p>
+            <input type="text" name="prix_produit" placeholder="Prix du Produit"><br />
+        <p>Description du produit :</p>
+            <input type="textarea" name="description_produit" placeholder="Décrivez le Produit"><br /><br />
             <!-- CREER LISTE DEROULANTE POUR CHOISIR LA CATEGORIE ET SOUS CATEGORIE DU PRODUIT -->
-            <input type="text" name="stock_produit" placeholder="En stock">
-        </form>
-    <!-- //Formulaire pour ajout d'image -->
-        <form name="img_up" action="gestion_article.php" method="POST" enctype="multipart/form-data">
+            <label>Catégorie du produit :</label><br /><br />
+            <select name="Categorie">
+            <?php $pageGestion->selectCategory(); ?>
+            </select><br /><br />
+
+            <label>Sous catégorie du produit :</label><br /><br />
+            <select name="SCategorie">
+            <?php $pageGestion->selectSCategory(); ?>
+            </select><br /><br />
+        <p>Qtt. Produits en Stock :</p>
+            <input type="text" name="stock_produit" placeholder="En stock"><br /><br />
             <input type="file" name="img"><br /><br />
             <input type="submit" name="upload" value="Upload">
         </form>
+    <!-- //Formulaire pour ajout d'image -->
     <?php if(isset($_POST["upload"])){
-        $pageGestion->ajoutImgArticle();
+        $pageGestion->ajoutProduitBdd();
     }
     ?>
     </main>
