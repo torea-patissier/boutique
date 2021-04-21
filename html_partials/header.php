19ba0971fcea2e6d9b74fa43f7139689f7fd9e1b
nuno.barbosa <nuno.barbosa@laplateforme.io>
2021-04-21 10:17:28 +0200

    Update header

--- html_partials/header.php
+++ html_partials/header.php
@@ -10,6 +10,22 @@
 
     <title>Document</title>
 </head>
+
+<?php
+
+if(!isset($_SESSION['user'])){
+    $id_droits = 0;
+}else{
+    $id_droits = $_SESSION['user']['id_droits'];
+}
+
+?>
+<body>
+
+ <!-- Navbar pour un utilisateur qui est connecté et est un ADMIN-->
+<?php
+if($id_droits == 2){
+?>
 <header>
         <div class="btn-navigation">
             <div class="barre"></div>
@@ -18,14 +34,67 @@
         </div>
         <div class="navigation">
             <ul>
-                    <li><a class="navlink" href="index.php">Accueil</a></li>
-                    <li><a class="navlink" href="Produit/produit.php">Produits</a></li>
-                    <li><a class="navlink" href="Contact/contact.php">Contact</a></li>
-                    <li><a class="navlink" href="Qui_sommes_nous/infos.php">Qui sommes nous</a></li>
-                    <li><a class="navlink" href="FAQ/faq.php">FAQ</a></li>
-                    <li><a class="navlink" href="Inscription/inscription.php">Inscription</a></li>
-                    <li><a class="navlink" href="Connexion/connexion.php">Connexion</a></li>
+                <li><a class="navlink" href="../index.php">Accueil</a></li>
+                <li><a class="navlink" href="../Produit/produit.php">Produit</a></li>
+                <li><a class="navlink" href="../Produits/produits.php">Produits</a></li>
+                <li><a class="navlink" href="../Contact/contact.php">Contact</a></li>
+                <li><a class="navlink" href="../Qui_sommes_nous/infos.php">Qui sommes nous</a></li>
+                <li><a class="navlink" href="../FAQ/faq.php">FAQ</a></li>
+                <li><a class="navlink" href="../Profil/profil.php">Profil</a></li>
+                <li><a class="navlink" href="../Admin/admin.php">Admin</a></li>
+                <li><a class="navlink" href="../Produits/panier.php">Panier</a></li>
             </ul>
         </div>
     </header>
-        
+    <?php }?> 
+
+<!-- Navbar pour un utilisateur qui est connecté-->
+<?php
+if($id_droits == 1){
+?>
+<header>
+        <div class="btn-navigation">
+            <div class="barre"></div>
+            <div class="barre"></div>
+            <div class="barre"></div>
+        </div>
+        <div class="navigation">
+            <ul>
+                <li><a class="navlink" href="../index.php">Accueil</a></li>
+                <li><a class="navlink" href="../Produit/produit.php">Produit</a></li>
+                <li><a class="navlink" href="../Produits/produits.php">Produits</a></li>
+                <li><a class="navlink" href="../Contact/contact.php">Contact</a></li>
+                <li><a class="navlink" href="../Qui_sommes_nous/infos.php">Qui sommes nous</a></li>
+                <li><a class="navlink" href="../FAQ/faq.php">FAQ</a></li>
+                <li><a class="navlink" href="../Profil/profil.php">Profil</a></li>
+                <li><a class="navlink" href="../Produits/panier.php">Panier</a></li>
+            </ul>
+        </div>
+    </header>
+<?php }?>
+
+
+
+<!-- Navbar pour un utilisateur qui n'est pas connecté-->
+<?php
+if($id_droits == 0){
+?>
+<header>
+        <div class="btn-navigation">
+            <div class="barre"></div>
+            <div class="barre"></div>
+            <div class="barre"></div>
+        </div>
+        <div class="navigation">
+            <ul>
+                <li><a class="navlink" href="../index.php">Accueil</a></li>
+                <li><a class="navlink" href="../Produit/produit.php">Produit</a></li>
+                <li><a class="navlink" href="../Produits/produits.php">Produits</a></li>
+                <li><a class="navlink" href="../Contact/contact.php">Contact</a></li>
+                <li><a class="navlink" href="../Qui_sommes_nous/infos.php">Qui sommes nous</a></li>
+                <li><a class="navlink" href="../FAQ/faq.php">FAQ</a></li>
+                <li><a class="navlink" href="../Connexion/connexion.php">Connexion</a></li>
+            </ul>
+        </div>
+    </header>
+<?php }?>
